bebfe2d33b253eab22ecc8037eb1983149f9816e
ITigor1985 <xromer85@gmail.com>
2022-02-16 17:16:05 +0200

    123

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,18 +1,17 @@
-const items = document.querySelectorAll('.slider-item');
+let items = document.querySelectorAll('.slider-item');
+let arr = [...items];
+
 const btnLeft = document.querySelector('.left');
 const btnRight = document.querySelector('.right');
 
 btnLeft.addEventListener("click", moveLeft);
-console.log(items) 
 
-function moveLeft(){
-    items.forEach((item, index, items) => {
-        if(item.classList.contains('active')){
-           item.classList.remove('active');
-           
-            
-            // items.push(lastElement).classList.add('active');
-        }
-        
-      })
+
+function moveLeft(){    
+let step=1;//шаг
+for(i=0;i<step;i++){
+    arr[0].classList.remove('active');
+    arr.unshift(arr.pop())} ;
+    arr[0].classList.add('active');
+    items=[...arr];    
 }
