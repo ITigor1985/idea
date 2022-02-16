988694268984edec41a4e1891348d7a9bb5375b7
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-02-16 22:15:34 +0200

    123

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,14 +4,29 @@ let arr = [...items];
 const btnLeft = document.querySelector('.left');
 const btnRight = document.querySelector('.right');
 
-btnLeft.addEventListener("click", moveLeft);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
 
-
-function moveLeft(){    
-let step=1;//шаг
-for(i=0;i<step;i++){
+function moveLeft() {
+  let step = 1; //шаг
+  for (i = 0; i < step; i++) {
     arr[0].classList.remove('active');
-    arr.unshift(arr.pop())} ;
-    arr[0].classList.add('active');
-    items=[...arr];    
+
+    arr[0].classList.add('no-active');
+    arr.unshift(arr.pop());
+  }
+  arr[0].classList.add('active');
+  arr[0].classList.remove('no-active');
+  items = [...arr];
+}
+
+function moveRight() {
+  let step = 1;
+  for (i = 0; i < step; i++) {
+    arr[0].classList.remove('active');
+    arr[0].classList.add('no-active');
+    arr.push(arr.shift());
+  }
+  arr[0].classList.add('active');
+  items = [...arr];
 }
